148d41e6a3af3e1d3cd069c9f0dd25aee457a471
kjjarzynowski@gmail.com <Kacper200)>
2022-03-04 22:46:40 +0100

    Created functions: 1)textToASCII - return int value of letter 2)ASCIItoBinary - return binary value of ASCII 3)translateText - translate text to binary

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+
+using namespace std;
+
+int textToASCII(char letter) {
+    return int(letter);
+}
+
+string ASCIItoBinary(int letter) {
+    string convertedLetter = "";
+
+    while(letter > 0) {
+        convertedLetter += to_string(letter % 2);
+        letter /= 2;
+        }
+
+    for(int i = 0; i < convertedLetter.size() / 2; ++i) {
+        swap(convertedLetter[i], convertedLetter[convertedLetter.size() - i - 1]);
+    }
+
+    return  convertedLetter;
+}
+
+string translateText(string text) {
+    string translatedText = "";
+
+    for(int i = 0; i < text.size(); ++i) {
+        translatedText += ASCIItoBinary(textToASCII(text[i]));
+        translatedText += " ";
+    }
+
+    return  translatedText;
+}
+
+int main() {
+    string text = "halo policja";
+
+    cout << translateText(text);
+
+    return 0;
+}
